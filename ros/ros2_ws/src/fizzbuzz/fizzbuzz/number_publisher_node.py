f758f52424cbdb76f61bc608fa418643635d736c
Luke Anger <anger.luke2004@gmail.com>
2025-01-07 14:28:30 -0500

    Changed the type from string to int64, runs correctly, good example on how to change types

--- ros/ros2_ws/src/fizzbuzz/fizzbuzz/number_publisher_node.py
+++ ros/ros2_ws/src/fizzbuzz/fizzbuzz/number_publisher_node.py
@@ -1,20 +1,20 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
+from std_msgs.msg import String, Int64
 
 class NumberPublisher(Node):
 
     def __init__(self):
         super().__init__('number_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(Int64, 'numbers', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg = Int64()
+        msg.data = self.i
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
