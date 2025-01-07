2c03596cb9c714bef2301035855c575b7b56e150
Luke Anger <anger.luke2004@gmail.com>
2025-01-07 13:51:10 -0500

    This is the actual starting point for the code

--- ros/ros2_ws/src/build/fizzbuzz/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/rick-sanchez/Desktop/MASLAB-clown-penis/ros/ros2_ws/src/install/fizzbuzz'
