789a7bdb692566c304fb884e4c92e4f349f4e729
Luke Anger <anger.luke2004@gmail.com>
2025-01-07 20:44:58 -0500

    complete reconstructure for the example ros

--- ros/ros2_ws/run.sh
+++
@@ -1,4 +0,0 @@
-# IMPORTANT NOTE: This does not keep the source outside of this file so if u run manually start by setting this source
-source install/setup.bash
-colcon build
-ros2 run fizzbuzz number_publisher
