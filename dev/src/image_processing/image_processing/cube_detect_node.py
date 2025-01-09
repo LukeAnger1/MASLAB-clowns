dc53f2d6e5ae5f38adaac1c99e223a9c0e35c0b1
Luke <anger.luke2004Agmail.com>
2025-01-08 23:14:06 -0500

    Added the main so that when it is called it runs

--- dev/src/image_processing/image_processing/cube_detect_node.py
+++ dev/src/image_processing/image_processing/cube_detect_node.py
@@ -29,5 +29,15 @@ class CubeDetect(Node):
         cube_image_msg = self.bridge.cv2_to_compressed_imgmsg(frame)
         self.cube_image_pub.publish(cube_image_msg)
 
-def main():
-    pass
+def main(args=None):
+    rclpy.init(args=args)
+
+    cube_detect = CubeDetect()
+
+    rclpy.spin(cube_detect)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    cube_detect.destroy_node()
+    rclpy.shutdown()
