fbd6fc2d627e80aac6342e4cf663c2980f2e5953
Luke <anger.luke2004Agmail.com>
2025-01-09 14:02:11 -0500

    It is loggging the calling here

--- dev/src/image_processing/image_processing/cube_detect_node.py
+++ dev/src/image_processing/image_processing/cube_detect_node.py
@@ -16,12 +16,26 @@ class CubeDetect(Node):
     # Object width (meters)
     OBJECT_WIDTH = 8.48e-2
 
+    last_frame = None
+
     def __init__(self):
         self.bridge = CvBridge()
         super().__init__('cube_detect')
 
         self.image_sub = self.create_subscription(CompressedImage, "image_raw/compressed", self.image_callback, 10)
         self.cube_image_pub = self.create_publisher(CompressedImage, "cube_image/compressed", 10)
+
+    def print_predicted_distances(self):
+        # Note: This is what we should fine tune to make our block detection better
+
+        if self.last_frame is None:
+            return
+
+        distance, x, y, w, h = self.compute_distance(self.last_frame)
+
+        self.get_logger().info(f'the distance is {distance} the x is {x} the y is {y} the w is {w} the h is {h}\n')
+
+        # Note: This is what we should fine tune to make our block detection better  
 
     def compute_distance(self, image):
         """
@@ -61,15 +75,11 @@ class CubeDetect(Node):
         return None, None, None, None, None
 
     def image_callback(self, msg: CompressedImage):
-        frame = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+        frame = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")  
 
-        # Note: This is what we should fine tune to make our block detection better
+        self.get_logger().info("calling in here")
 
-        distance, x, y, w, h = self.compute_distance(frame)
-
-        self.get_logger().info(f'the distance is {distance} the x is {x} the y is {y} the w is {w} the h is {h}\n')
-
-        # Note: This is what we should fine tune to make our block detection better    
+        self.print_predicted_distances()
 
         cube_image_msg = self.bridge.cv2_to_compressed_imgmsg(frame)
         self.cube_image_pub.publish(cube_image_msg)
