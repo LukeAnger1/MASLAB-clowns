84ae83113d034ec33a0211907d0e8640749f53e7
Luke <anger.luke2004Agmail.com>
2025-01-09 11:51:12 -0500

    Untested distnace estimater. Needs to be fine tuned

--- dev/src/image_processing/image_processing/cube_detect_node.py
+++ dev/src/image_processing/image_processing/cube_detect_node.py
@@ -10,6 +10,12 @@ from std_msgs.msg import String
 
 class CubeDetect(Node):
 
+    # Calibration parameter (focal length over sensor width)
+    CALIBRATION = 1
+
+    # Object width (meters)
+    OBJECT_WIDTH = 8.48e-2
+
     def __init__(self):
         self.bridge = CvBridge()
         super().__init__('cube_detect')
@@ -17,12 +23,51 @@ class CubeDetect(Node):
         self.image_sub = self.create_subscription(CompressedImage, "image_raw/compressed", self.image_callback, 10)
         self.cube_image_pub = self.create_publisher(CompressedImage, "cube_image/compressed", 10)
 
+    def compute_distance(self, image):
+        """
+        Compute the distance of the block, given an image
+        """
+        # Convert the image to HSV
+        hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        # Apply a mask to the image
+        mask = cv2.inRange(
+            hsv_image,
+            np.array([60, 100, 100]),
+            np.array([90, 200, 200])
+        )
+
+        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Check contours
+        if contours:
+            # Get the largest contour
+            max_contour = max(contours, key=cv2.contourArea)
+
+            # Find the bounding rectangle
+            x, y, w, h = cv2.boundingRect(max_contour)
+
+            # Calculate the contour width, in pixels
+            width = np.sqrt(cv2.contourArea(max_contour))
+
+            # Get the frame width, in pixels
+            frame_width = image.shape[1]
+        
+            # Calculate the object's distance from its width (centimeters)
+            distance = self.OBJECT_WIDTH * frame_width / width * self.CALIBRATION * 100
+
+            return distance, x, y, w, h
+        
+        return None, None, None, None, None
+
     def image_callback(self, msg: CompressedImage):
         frame = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
 
         # Note: This is what we should fine tune to make our block detection better
 
-        
+        distance, x, y, w, h = self.compute_distance(frame)
+
+        self.get_logger().info(f'the distance is {distance} the x is {x} the y is {y} the w is {w} the h is {h}\n')
 
         # Note: This is what we should fine tune to make our block detection better    
 
