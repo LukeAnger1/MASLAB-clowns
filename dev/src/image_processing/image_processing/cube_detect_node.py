c67ed29471c3298490cadc9ba184425d5d741686
Luke <anger.luke2004Agmail.com>
2025-01-09 14:05:25 -0500

    printing predicted distances but they are all None

--- dev/src/image_processing/image_processing/cube_detect_node.py
+++ dev/src/image_processing/image_processing/cube_detect_node.py
@@ -16,8 +16,6 @@ class CubeDetect(Node):
     # Object width (meters)
     OBJECT_WIDTH = 8.48e-2
 
-    last_frame = None
-
     def __init__(self):
         self.bridge = CvBridge()
         super().__init__('cube_detect')
@@ -25,13 +23,10 @@ class CubeDetect(Node):
         self.image_sub = self.create_subscription(CompressedImage, "image_raw/compressed", self.image_callback, 10)
         self.cube_image_pub = self.create_publisher(CompressedImage, "cube_image/compressed", 10)
 
-    def print_predicted_distances(self):
+    def print_predicted_distances(self, frame):
         # Note: This is what we should fine tune to make our block detection better
 
-        if self.last_frame is None:
-            return
-
-        distance, x, y, w, h = self.compute_distance(self.last_frame)
+        distance, x, y, w, h = self.compute_distance(frame)
 
         self.get_logger().info(f'the distance is {distance} the x is {x} the y is {y} the w is {w} the h is {h}\n')
 
@@ -77,9 +72,7 @@ class CubeDetect(Node):
     def image_callback(self, msg: CompressedImage):
         frame = self.bridge.compressed_imgmsg_to_cv2(msg, "bgr8")  
 
-        self.get_logger().info("calling in here")
-
-        self.print_predicted_distances()
+        self.print_predicted_distances(frame)
 
         cube_image_msg = self.bridge.cv2_to_compressed_imgmsg(frame)
         self.cube_image_pub.publish(cube_image_msg)
