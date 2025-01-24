d1663a5598c46813a921aab528dce27c750b58dd
Luke <anger.luke2004Agmail.com>
2025-01-24 13:06:43 -0500

    made temporary black data points for testing (will have to redo with permanent mount later)

---
+++ camera_test/black/data_points.py
@@ -0,0 +1,11 @@
+# Example data from /image_raw/compressed_mouse_left
+PTS_IMAGE_PLANE = [[501, 118],
+                   [107, 123],
+                   [171, 336]
+                   ]
+
+# Example data from corresponding point in real world
+PTS_GROUND_PLANE = [[4+3/8, 8+9/16],
+                    [-(4+3/16), 8+3/4],
+                    [-(2+5/8), 4+1/32]
+                    ]
