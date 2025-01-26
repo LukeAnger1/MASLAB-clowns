fdcbdd8e3fb2e83cdf369a4701b3d9e9cb8eeec7
Luke <anger.luke2004Agmail.com>
2025-01-26 15:29:00 -0500

    builds but need to have import of raven fix on the pi

--- dev/src/motor_control/motor_control/drive_control.py
+++ dev/src/motor_control/motor_control/drive_control.py
@@ -3,6 +3,14 @@ from rclpy.node import Node
 
 from decision_interfaces.msg import GoalDestination
 
+from raven.raven import Raven
+raven_board = Raven()
+
+import math
+
+# This constant is how much to scale the angles by
+RADIANS_MULTIPLIER = 5
+
 class DriveNode(Node):
     def __init__(self):
         super().__init__('drive_node')
@@ -10,25 +18,43 @@ class DriveNode(Node):
 
         self.number_sub = self.create_subscription(GoalDestination, "motor_control/goal_destination", self.update_motors, 10)
 
+        # These are values to control driving at basic level
+        self.drive = False
+        self.angle = 0
+        self.goal_speed = 12
+
+        self.run_motors()
+
     def update_motors(self, msg):
-        # # this function is called whenever a number is received.
 
-        # number = msg.data 
+        goal_x = msg.x
+        goal_y = msg.y
 
-        # fizzbuzz_str = self.fizzbuzz(number)
-        # # loginfo to print the string to the terminal
-        # self.get_logger().info(fizzbuzz_str)
+        self.angle = math.atan(goal_x/goal_y)
 
-        # fizzbuzz_msg = FizzBuzz()
-        # fizzbuzz_msg.fizzbuzz = fizzbuzz_str
-        # fizzbuzz_msg.fizz_ratio = 0 # TODO fill in this value
-        # fizzbuzz_msg.buzz_ratio = 0 # TODO fill in this value
-        # fizzbuzz_msg.fizzbuzz_ratio = 0 # TODO fill in this value
-        # fizzbuzz_msg.number_total = 0 # TODO fill in this value
+    def run_motors(self):
 
-        # # publish the message
-        # self.fizzbuzz_pub.publish(fizzbuzz_msg)
-        pass
+        # Set one motor
+        raven_board.set_motor_mode(Raven.MotorChannel.CH4, Raven.MotorMode.DIRECT)
+
+        raven_board.set_motor_torque_factor(Raven.MotorChannel.CH4, 50)
+
+        # Set the other motor
+        raven_board.set_motor_mode(Raven.MotorChannel.CH5, Raven.MotorMode.DIRECT)
+
+        raven_board.set_motor_torque_factor(Raven.MotorChannel.CH5, 50)
+
+        while True:
+            angle_measurement = self.angle * RADIANS_MULTIPLIER
+
+            if self.drive:
+                # We are driving
+                raven_board.set_motor_speed_factor(Raven.MotorChannel.CH4, self.goal_speed - angle_measurement)
+                raven_board.set_motor_speed_factor(Raven.MotorChannel.CH5, self.goal_speed + angle_measurement, reverse=True)
+            else:
+                # We are not driving
+                raven_board.set_motor_speed_factor(Raven.MotorChannel.CH4, 0)
+                raven_board.set_motor_speed_factor(Raven.MotorChannel.CH5, 0, reverse=True)
 
 def main(args=None):
     rclpy.init()
