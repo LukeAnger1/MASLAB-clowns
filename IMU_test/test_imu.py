1ad20dab1169f3e27801a79fe6f4b5a92be726ea
Team 17 <maslab-2025-team-17@mit.edu>
2025-01-26 14:29:34 -0500

    working test, added print statements and the import that was missing

--- IMU_test/test_imu.py
+++ IMU_test/test_imu.py
@@ -1,5 +1,6 @@
 from icm42688 import ICM42688
 import board
+import busio
 
 spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
@@ -11,6 +12,9 @@ spi.configure(baudrate=5000000)
 imu = ICM42688(spi)
 imu.begin()
 
-accel, gyro = imu.get_data()
-# Returned linear acceleration is a tuple of (X, Y, Z) m/s^2
-# Returned gyroscope reading is a tuple of (X, Y, Z) radian/s
+while True:
+    accel, gyro = imu.get_data()
+    # Returned linear acceleration is a tuple of (X, Y, Z) m/s^2
+    # Returned gyroscope reading is a tuple of (X, Y, Z) radian/s
+
+    print(f'accel is {accel} while the gyro is {gyro}')
