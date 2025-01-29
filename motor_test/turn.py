00630841cc04effa15cf2f103f98dc8993475438
Team 17 <maslab-2025-team-17@mit.edu>
2025-01-29 16:26:58 -0500

    fine tuned the motors on the new robot

--- motor_test/turn.py
+++ motor_test/turn.py
@@ -9,20 +9,20 @@ radians = 0
 RADIANS_MULTIPLIER = 5
 
 # This constant is the goal speed
-GOAL_SPEED = 10
+GOAL_SPEED = 20
 
 ANGLE_MEASUREMENT = radians * RADIANS_MULTIPLIER
 
 # Set one motor
 raven_board.set_motor_mode(Raven.MotorChannel.CH4, Raven.MotorMode.DIRECT)
 
-raven_board.set_motor_torque_factor(Raven.MotorChannel.CH4, 50)
+raven_board.set_motor_torque_factor(Raven.MotorChannel.CH4, 85)
 raven_board.set_motor_speed_factor(Raven.MotorChannel.CH4, GOAL_SPEED - ANGLE_MEASUREMENT)
 
 # Set the other motor
 raven_board.set_motor_mode(Raven.MotorChannel.CH5, Raven.MotorMode.DIRECT)
 
-raven_board.set_motor_torque_factor(Raven.MotorChannel.CH5, 50)
+raven_board.set_motor_torque_factor(Raven.MotorChannel.CH5, 85)
 raven_board.set_motor_speed_factor(Raven.MotorChannel.CH5, GOAL_SPEED + ANGLE_MEASUREMENT, reverse=True)
 
 while True:
